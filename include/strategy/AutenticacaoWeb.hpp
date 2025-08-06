f7e6c244344259090b41dab0c413562166873343
AquaMouzinho <42475699+AquaMouzinho@users.noreply.github.com>
2025-08-05 21:34:23 -0300

    Atualizacao Autenticacao Web

--- include/strategy/AutenticacaoWeb.hpp
+++ include/strategy/AutenticacaoWeb.hpp
@@ -2,21 +2,23 @@
 #pragma once
 #include "AutenticacaoStrategy.hpp"
 #include "../repositorios/UsuarioRepository.hpp"
-#include "../utils/JWTUtils.hpp" // Supondo que você terá utilitários JWT
+// #include "../utils/JWTUtils.hpp"
 
-class AutenticacaoWeb : public AutenticacaoStrategy {
+class AutenticacaoWeb : public AutenticacaoStrategy
+{
 private:
-    UsuarioRepository* usuarioRepository;
+    UsuarioRepository *usuarioRepository;
 
 public:
-    AutenticacaoWeb(UsuarioRepository* usuarioRepo) : usuarioRepository(usuarioRepo) {}
+    AutenticacaoWeb(UsuarioRepository *usuarioRepo) : usuarioRepository(usuarioRepo) {}
 
-    Usuario* autenticar(const std::string& token, const std::string& /* não usado */) override {
-        // Implemente a validação do token JWT
-        std::string matricula = JWTUtils::extrairMatricula(token);
-        if (!matricula.empty()) {
-            return usuarioRepository->buscarUsuarioPorMatricula(matricula);
-        }
+    Usuario *autenticar(const std::string &token, const std::string & /* não usado */) override
+    {
+        // Implementar a validação do token JWT
+        // std::string matricula = JWTUtils::extrairMatricula(token);
+        // if (!matricula.empty()) {
+        //    return usuarioRepository->buscarUsuarioPorMatricula(matricula);
+        //}
         return nullptr;
     }
 };
